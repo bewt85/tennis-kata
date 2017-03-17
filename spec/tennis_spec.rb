d6bc10d9968d533afa3f6b93b6b9ad24ee6b88f6
Dmitry Maksyoma <ledestin@gmail.com>
2017-03-17 22:20:05 +1300

    Add failing test

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -4,6 +4,11 @@ describe Tennis do
   specify 'players are love-all when game starts' do
     expect(subject.score).to eq '0:0'
   end
+
+  specify "player 1 wins a point" do
+    subject.player1_wins_a_point
+    expect(subject.score).to eq "15:0"
+  end
 end
 
 # love-all
